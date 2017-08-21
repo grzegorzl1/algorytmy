5e42e307886cda4b8c1ba17f78a25845e7b79c01
grzegorzl1 <grzegorz.lezucha1@gmail.com>
2017-08-21 17:27:33 +0200

    algorytm

---
+++ 12_witaj.rb
@@ -0,0 +1,21 @@
+puts "Podaj liczbę:"
+n = gets.to_i
+
+loop do
+  puts "Witaj"
+  break if n == 1
+  n = n - 1
+end
+
+
+
+# lub
+
+# n = 10
+# x = 1
+#
+# loop do
+#   puts "Witaj"
+#   break if n == x
+#   x = x + 1
+# end
